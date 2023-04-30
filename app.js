00d1a4722de335291da92717c0dba385839c9b9e
Bikash <acbikash13@gmail.com>
2023-04-30 17:17:38 -0400

    mongo db client.connect still not executing

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-
 const express=require('express')
 const bodyParser = require('body-parser')
 const cookieParser=require('cookie-parser');
 const app=express();
 const port= 8080
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = "mongodb+srv://acharyab3:Iamcosmos@acharyab3.wg17b1q.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://acharyab2:Iamcosmos@acharyab2.wg17b1q.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1});
 const bcrypt=require('bcrypt')
 const jwt=require('jsonwebtoken')
@@ -20,22 +19,22 @@ app.use(cookieParser());
 
 // 
 
-client.connect(function(err,db){
-	if (err){
-        console.log("error")
-        console.error(err);
-    }
-  console.log("Just before")
-	console.log('Connected to database')
-	database = db.db('BingoGame')
-	database.collection("users").find({}).toArray(err,result)
-	app.listen(port,async() => {
-	  console.log(`Example app listening on port ${port}`)
-	})
-  client.close();
+// client.connect(function(err,db){
+// 	if (err){
+//         console.log("error")
+//         console.error(err);
+//     }
+//   console.log("Just before")
+// 	console.log('Connected to database')
+// 	database = db.db('BingoGame')
+// 	database.collection("users").find({}).toArray(err,result)
+// 	app.listen(port,async() => {
+// 	  console.log(`Example app listening on port ${port}`)
+// 	})
+//   client.close();
 
-}
-);
+// }
+// );
 
 
 
@@ -50,22 +49,22 @@ client.connect(function(err,db){
 //     deprecationErrors: true,
 //   }
 // }); 
-
-// async function run() {
-//   try {
-//     // Connect the client to the server	(optional starting in v4.7)
-//     await client.connect();
-//     // Send a ping to confirm a successful connection
-//     var database = await client.db("BingoGame").command({ ping: 1 });
-//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     app.listen(port,async () => {
-// 		console.log(`Example code running on port ${port}`);
-// 	})
-//   }
-// }
-// run().catch(console.dir);
+let database = null
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    database = await client.db("BingoGame");
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    app.listen(port,async () => {
+		console.log(`Example code running on port ${port}`);
+	})
+  }
+}
+run().catch(console.dir);
 async function checkUser(token){
 	let result=await database.collection('users').find({jwt:token},{_id:1}).toArray();
 	if(result.length>0){
@@ -77,10 +76,18 @@ async function checkUser(token){
 
 
 app.post('/api/auth/signup',(req,res)=>{
-	console.log(req.body)
-	database.collection('users').find({email:res.body.email}).toArray(function(err, result){
-		if (err) throw err
-		if(result.length>0) res.status(406).json({message:'User already exists'})
+	console.log(req.body);
+	let email = (req.body.email);
+	console.log(database);
+	console.log("after logging to database");
+	database.collection('users').find({username:email}).toArray(function(err, result){
+		if (err) {
+			console.log(err)
+		}
+		console.log("Inside the database.collection")
+		if(result.length>0) {
+			console.log("User Exists!")
+			res.status(406).json({message:'User already exists'});}
 		else{
 			req.body.password=bcrypt.hashSync(req.body.password,salt).replace(`${salt}.`,'')
 			database.collection('users').insertOne(req.body,function(err,result){
@@ -136,6 +143,7 @@ app.get('/games/users/:user/playerState',(res,req)=>{
 })
 app.get('/api/auth/signout',(req,res)=>{
 	res.json(req.body)
+	res.sendFile(__dirname + "/public/FrontEnd/html/login.html");
 })
 app.get('/api/request',(req,res)=>{
 	res.json(req.body)
